3094485831c425bacad2e8fa923e4fa5683b0b49
tieudat <hi.tieudat@gmail.com>
2023-03-08 21:20:18 +0700

    Convert Face sales order line

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -1,7 +1,7 @@
 SELECT 
-   order_line_id as sales_order_line_key,
-   quantity,
-   unit_price,
-   quantity*unit_price AS gross_amount,
+   CAST(order_line_id as Integer) as sales_order_line_key,
+   CAST(quantity as Integer) as quantity,
+   CAST(unit_price as Numeric) as unit_price,
+   CAST(quantity as Integer) * CAST(unit_price as Numeric) AS gross_amount,
    stock_item_id as product_key
 FROM `vit-lam-data.wide_world_importers.sales__order_lines`
