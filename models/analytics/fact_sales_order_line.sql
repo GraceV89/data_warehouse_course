16e7f06254bcd46ec6bfd75ab9ea6dd433d691bb
tieudat <hi.tieudat@gmail.com>
2023-03-09 10:09:50 +0700

    Convert fact_sales_order_line

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -1,7 +1,8 @@
 SELECT 
    CAST(order_line_id as Integer) as sales_order_line_key,
+   CAST(stock_item_id as Integer) product_key,
    CAST(quantity as Integer) as quantity,
    CAST(unit_price as Numeric) as unit_price,
    CAST(quantity as Integer) * CAST(unit_price as Numeric) AS gross_amount,
-   stock_item_id as product_key
+   
 FROM `vit-lam-data.wide_world_importers.sales__order_lines`
