c0899aa7c1aed019aaa7e86e87b038df0f92f148
tieudat <hi.tieudat@gmail.com>
2023-03-09 13:29:40 +0700

    fact_sales_order_line added sales_order_key

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -6,6 +6,7 @@ WITH fact_sales_order_line__source AS(
 fact_sales_order_line__rename_column AS(
   SELECT 
    order_line_id as sales_order_line_key
+   ,order_id as sales_order_key
    ,stock_item_id as product_key
    ,quantity as quantity
    ,unit_price as unit_price
@@ -17,6 +18,7 @@ FROM fact_sales_order_line__source
 fact_sales_order_line__convert_type AS(
   SELECT 
    CAST(sales_order_line_key as Integer) as sales_order_line_key
+   ,CAST(sales_order_key as Integer) as sales_order_key
    ,CAST( product_key as Integer) as product_key
    ,CAST(quantity as Integer) as quantity
    ,CAST(unit_price as Numeric) as unit_price
@@ -27,6 +29,7 @@ fact_sales_order_line__convert_type AS(
 
 SELECT 
    sales_order_line_key
+   ,sales_order_key
    ,product_key
    ,quantity
    ,unit_price
