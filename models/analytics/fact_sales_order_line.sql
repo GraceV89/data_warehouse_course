d217c95c604ba0c5a30510b2fccb93f8028deef1
tieudat <hi.tieudat@gmail.com>
2023-03-09 11:04:49 +0700

    CTE_fact_sales_order_line

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -1,8 +1,35 @@
-SELECT 
-   CAST(order_line_id as Integer) as sales_order_line_key,
-   CAST(stock_item_id as Integer) product_key,
-   CAST(quantity as Integer) as quantity,
-   CAST(unit_price as Numeric) as unit_price,
-   CAST(quantity as Integer) * CAST(unit_price as Numeric) AS gross_amount,
+WITH fact_sales_order_line__source AS(
+  SELECT *
+  FROM  `vit-lam-data.wide_world_importers.sales__order_lines`
+)
+, 
+fact_sales_order_line__rename_column AS(
+  SELECT 
+   order_line_id as sales_order_line_key
+   ,stock_item_id as product_key
+   ,quantity as quantity
+   ,unit_price as unit_price
+   ,quantity  * unit_price  as gross_amount
    
-FROM `vit-lam-data.wide_world_importers.sales__order_lines`
+FROM fact_sales_order_line__source
+)
+,
+fact_sales_order_line__convert_type AS(
+  SELECT 
+   CAST(sales_order_line_key as Integer) as sales_order_line_key
+   ,CAST( product_key as Integer) as product_key
+   ,CAST(quantity as Integer) as quantity
+   ,CAST(unit_price as Numeric) as unit_price
+   ,CAST(quantity as Integer) * CAST(unit_price as Numeric) as gross_amount
+
+   FROM fact_sales_order_line__rename_column
+)
+
+SELECT 
+   sales_order_line_key
+   ,product_key
+   ,quantity
+   ,unit_price
+   ,quantity * unit_price as gross_amount
+   
+FROM fact_sales_order_line__convert_type
