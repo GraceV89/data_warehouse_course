fce2555981ce08a59417ac527cf11d782f621e83
tieudat <hi.tieudat@gmail.com>
2023-03-11 17:33:03 +0700

    Left Join dim_product vs dim_supplier

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -10,6 +10,7 @@ SELECT
 stock_item_id   as product_key 
 ,stock_item_name as product_name 
 ,brand as brand_name
+,supplier_id as supplier_key
  FROM dim_procduct__source  
 )
 
@@ -18,11 +19,16 @@ SELECT
 CAST(product_key AS INTEGER)  as product_key 
 ,CAST( product_name AS STRING) as product_name 
 ,CAST(brand_name AS STRING) as brand_name
+,CAST(supplier_key AS INTEGER) as supplier_key
  FROM dim_product__rename_column 
 )
 
 SELECT
-product_key 
-,product_name 
-,brand_name
-FROM dim_product__cast_type
+dim_product.product_key 
+,dim_product.product_name 
+,dim_product.supplier_key
+,dim_supplier.supplier_name
+,dim_product.brand_name
+FROM dim_product__cast_type as dim_product
+LEFT JOIN {{ref('dim_supplier')}} as dim_supplier
+ON dim_product.supplier_key=dim_supplier.supplier_key
