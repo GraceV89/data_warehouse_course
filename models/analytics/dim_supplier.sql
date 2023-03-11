8bed4df10de578693c629c3f8950b531d7544904
tieudat <hi.tieudat@gmail.com>
2023-03-11 16:49:32 +0700

    Create dim_supplier

--- models/analytics/dim_supplier.sql
+++ models/analytics/dim_supplier.sql
@@ -1,3 +1,29 @@
+WITH dim_supplier__source AS(
 SELECT 
   *
 FROM `vit-lam-data.wide_world_importers.purchasing__suppliers`
+)
+,
+
+dim_supplier__rename_column AS(
+ SELECT
+ supplier_id AS supplier_key
+ ,supplier_name AS supplier_name
+ FROM dim_supplier__source
+)
+,
+
+dim_supplier__convert_type AS(
+  SELECT
+  CAST(supplier_key AS INTEGER) AS supplier_key
+  ,CAST (supplier_name AS STRING) AS supplier_name
+  FROM dim_supplier__rename_column
+)
+
+SELECT
+dim_supplier__convert_type.supplier_key
+,dim_supplier__convert_type.supplier_name
+FROM dim_supplier__convert_type
+
+
+
