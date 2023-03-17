09935a6dd41ef5e4639e1c36908a6f5aeab41690
tieudat <hi.tieudat@gmail.com>
2023-03-17 15:20:41 +0700

    add IsChillerStock to dim_product

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -6,29 +6,32 @@ FROM `vit-lam-data.wide_world_importers.warehouse__stock_items`
 
 , 
 dim_product__rename_column AS(
-SELECT 
-stock_item_id   as product_key 
-,stock_item_name as product_name 
-,brand as brand_name
-,supplier_id as supplier_key
- FROM dim_procduct__source  
+  SELECT 
+    stock_item_id   as product_key 
+    ,stock_item_name as product_name 
+    ,brand as brand_name
+    ,supplier_id as supplier_key
+    ,is_chiller_stock as is_chiller_stock 
+  FROM dim_procduct__source  
 )
 
 ,dim_product__cast_type AS(
-SELECT 
-CAST(product_key AS INTEGER)  as product_key 
-,CAST( product_name AS STRING) as product_name 
-,CAST(brand_name AS STRING) as brand_name
-,CAST(supplier_key AS INTEGER) as supplier_key
- FROM dim_product__rename_column 
+  SELECT 
+    CAST(product_key AS INTEGER)  as product_key 
+    ,CAST( product_name AS STRING) as product_name 
+    ,CAST(brand_name AS STRING) as brand_name
+    ,CAST(supplier_key AS INTEGER) as supplier_key
+    ,CAST(is_chiller_stock AS BOOLEAN) as is_chiller_stock
+  FROM dim_product__rename_column 
 )
 
-SELECT
-dim_product.product_key 
-,dim_product.product_name 
-,dim_product.supplier_key
-,dim_supplier.supplier_name
-,dim_product.brand_name
-FROM dim_product__cast_type as dim_product
+  SELECT
+    dim_product.product_key 
+    ,dim_product.product_name 
+    ,dim_product.supplier_key
+    ,dim_supplier.supplier_name
+    ,dim_product.brand_name
+    ,dim_product.is_chiller_stock
+  FROM dim_product__cast_type as dim_product
 LEFT JOIN {{ref('dim_supplier')}} as dim_supplier
-ON dim_product.supplier_key=dim_supplier.supplier_key
+  ON dim_product.supplier_key=dim_supplier.supplier_key
