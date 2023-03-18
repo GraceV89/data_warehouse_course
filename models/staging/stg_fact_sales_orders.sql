bffff5b85df261c4fffc091e5d59b1bd0a32350c
tieudat <hi.tieudat@gmail.com>
2023-03-18 15:51:05 +0700

    ok

--- models/staging/stg_fact_sales_orders.sql
+++ models/staging/stg_fact_sales_orders.sql
@@ -8,6 +8,7 @@ fact_sales_orders__rename_column AS(
    order_id as sales_order_key
    ,customer_id as customer_key
    ,picked_by_person_id as picked_by_person_key
+   ,order_date  
   FROM fact_sales_orders__source
 )
 ,
@@ -16,6 +17,7 @@ fact_sales_orders_convert_type AS(
   CAST (sales_order_key as Integer) as sales_order_key
   , CAST (customer_key as Integer) as customer_key
   ,CAST (picked_by_person_key as Integer) as picked_by_person_key
+  ,CAST (order_date as DATE) as order_date
   FROM fact_sales_orders__rename_column
 )
 
@@ -23,4 +25,5 @@ SELECT
 sales_order_key
 ,customer_key
 ,COALESCE(picked_by_person_key, 0) AS picked_by_person_key
+,order_date
  FROM fact_sales_orders_convert_type
