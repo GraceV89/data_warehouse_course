2f2b47a11ce74ae03ace3d218245d94d8f8fb54a
tieudat <hi.tieudat@gmail.com>
2023-03-18 12:41:20 +0700

    Them cot cho bang dim_person

--- models/analytics/dim_person.sql
+++ models/analytics/dim_person.sql
@@ -1,7 +1,7 @@
 WITH dim_person_source AS(
-SELECT 
-  *
-FROM `vit-lam-data.wide_world_importers.application__people`
+    SELECT 
+      *
+    FROM `vit-lam-data.wide_world_importers.application__people`
 )
 ,
 
@@ -19,11 +19,24 @@ dim_person__convert_type AS(
       ,CAST (full_name AS STRING) AS full_name
   FROM dim_person__rename_column
 )
+,dim_person_add_undefined_row AS (
+  SELECT
+    person_key
+    ,full_name
+  FROM dim_person__convert_type 
+
+  UNION ALL
+  SELECT
+      0 AS person_key
+      ,'Undefine' AS full_name  
+      -- khong co FROM
+)
 
 SELECT
-    dim_person__convert_type.person_key
-    ,dim_person__convert_type.full_name
+    person_key
+   ,full_name
 FROM dim_person__convert_type
+
 
 
 
